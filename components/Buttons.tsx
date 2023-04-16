3fc21e38171369d7f34fbfcb67f16a70bbf7b5e5
Ashish Sharma <itsashishsharma1801@gmail.com>
2023-04-16 12:20:57 +0530

    Build Index page.

---
+++ components/Buttons.tsx
@@ -0,0 +1,43 @@
+import clsx from 'clsx';
+
+interface ButtonProps {
+  children: string;
+  className?: string;
+  onClickHandler?: () => void;
+}
+
+const baseStyle =
+  'group inline-flex items-center justify-center rounded py-2 px-7 font-semibold focus:outline-none';
+
+export function ButtonSolid({ children, className }: ButtonProps) {
+  return (
+    <button
+      className={clsx(
+        baseStyle,
+        className,
+        'bg-slate-800 text-slate-50 hover:bg-slate-700 hover:text-slate-100 active:bg-slate-800 active:text-slate-300 focus-visible:outline-slate-800 focus-visible:outline-2 focus-visible:outline-offset-2 '
+      )}
+    >
+      {children}
+    </button>
+  );
+}
+
+export function ButtonOutline({
+  children,
+  className,
+  onClickHandler,
+}: ButtonProps) {
+  return (
+    <button
+      onClick={onClickHandler}
+      className={clsx(
+        baseStyle,
+        className,
+        'ring-1 ring-slate-300 text-slate-700 hover:text-slate-800 hover:ring-slate-500 active:bg-slate-100 active:text-slate-600 focus-visible:outline-emerald-500 focus-visible:ring-slate-300'
+      )}
+    >
+      {children}
+    </button>
+  );
+}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,17 @@
 import Link from 'next/link';
 import { Logo } from './Icons';
+import { ButtonSolid } from './Buttons';
 
 function Navbar() {
   return (
-    <nav className='py-6 md:py-8'>
-      <Link href='/' className='block'>
+    <nav className='flex items-center justify-between py-6 md:py-8'>
+      <Link
+        href='/'
+        className='block focus:outline-none focus:ring-2 focus:ring-offset-2 focus:rounded focus:ring-slate-700'
+      >
         <Logo className='h-8 w-auto' />
       </Link>
+      <ButtonSolid>Sign in</ButtonSolid>
     </nav>
   );
 }
