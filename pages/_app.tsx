6ece0cc8ef5667b0a54885f9fe665561118da459
Ashish Sharma <itsashishsharma1801@gmail.com>
2023-04-16 10:59:12 +0530

    Use custom font.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,37 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import localFont from 'next/font/local';
+
+const averta = localFont({
+  src: [
+    {
+      path: '../public/fonts/AvertaStd-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/AvertaStd-Semibold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/AvertaStd-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/AvertaStd-ExtraBold.woff2',
+      weight: '800',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-averta',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${averta.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
